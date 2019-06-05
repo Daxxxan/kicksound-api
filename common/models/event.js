f6fabb52bd5676313d17c312dc69beb551a8b1cd
Unknown <alex-bad97@hotmail.fr>
2019-06-05 17:37:02 +0200

    Get event participants

--- common/models/event.js
+++ common/models/event.js
@@ -1,4 +1,22 @@
 'use strict';
 
 module.exports = function(Event) {
+  Event.getEventParticipants = function(id, cb) {
+    Event.findById(id, {include: {tickets: 'account'}},
+      function(err, instance) {
+        let result = [];
+        for (let counter = 0; counter < instance.tickets().length; counter++) {
+          result.push(instance.tickets()[counter].account());
+        }
+        cb(null, result);
+      });
+  };
+
+  Event.remoteMethod('getEventParticipants', {
+    accepts: {arg: 'id', type: 'number', http: {source: 'path'},
+      required: true, description: 'Event ID'},
+    returns: {arg: 'Accounts', type: 'string'},
+    http: {path: '/:id/participants', verb: 'get'},
+    description: 'Get all event\'s accounts',
+  });
 };
