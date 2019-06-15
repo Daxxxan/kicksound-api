55f52b054d9e7e7772b7682034213390476c0157
Unknown <alex-bad97@hotmail.fr>
2019-06-15 17:55:00 +0200

    Get events by followed users

--- common/models/account.js
+++ common/models/account.js
@@ -19,4 +19,25 @@ module.exports = function(Account) {
     http: {path: '/:id/eventParticipation', verb: 'get'},
     description: 'Get all events which user participate',
   });
+
+  Account.getEventByFollowedUser = function(id, cb) {
+    Account.findById(id, {include: {following: ['events']}},
+      function(err, instance) {
+        let result = [];
+        for (let i = 0; i < instance.following().length; i++) {
+          for (let j = 0; j < instance.following()[i].events().length; j++) {
+            result.push(instance.following()[i].events()[j]);
+          }
+        }
+        cb(null, result);
+      });
+  };
+
+  Account.remoteMethod('getEventByFollowedUser', {
+    accepts: {arg: 'id', type: 'number', http: {source: 'path'},
+      required: true, description: 'User ID'},
+    returns: {type: 'array', root: 'true'},
+    http: {path: '/:id/eventByFollowedUser', verb: 'get'},
+    description: 'Get all events by followed users',
+  });
 };
