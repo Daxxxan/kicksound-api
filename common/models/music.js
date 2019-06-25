0bca264f9c665ea2226b4589babb19097bfdab8c
Unknown <alex-bad97@hotmail.fr>
2019-06-25 11:57:09 +0200

    Get user who like a music

--- common/models/music.js
+++ common/models/music.js
@@ -2,9 +2,16 @@
 
 module.exports = function(Music) {
   Music.getMusicByArtist = function(id, cb) {
-    Music.find({where: {accountId: id}, include: ['account',
-      'accountWhoLike']},
+    Music.find({where: {accountId: id}, include: ['account', 'accountWhoLike']},
       function(err, instance) {
+        instance = instance.map((i) => i.toJSON());
+        for (let i = 0; i < instance.length; i++) {
+          for (let j = 0; j < instance[i].accountWhoLike.length; j++) {
+            if (instance[i].accountWhoLike[j].id !== id) {
+              instance[i].accountWhoLike.splice(j, 1);
+            }
+          }
+        }
         cb(null, instance);
       });
   };
