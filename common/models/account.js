6a4197cb1ada815f023b00e426285b50a94fac23
Unknown <alex-bad97@hotmail.fr>
2019-07-06 01:33:45 +0200

    log test

--- common/models/account.js
+++ common/models/account.js
@@ -103,6 +103,7 @@ module.exports = function(Account) {
 
         for (let i = 0; i < instance.following.length; i++) {
           if (instance.following[i].type === 2) {
+            console.log(instance.following[i].highlight);
             for (let j = 0; j < instance.following[i].highlight.length; j++) {
               result.push(instance.following[i].highlight[j]);
             }
